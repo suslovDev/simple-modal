af2e2354cdf81e9d260655f8b9a6788ccee73cf1
SuslovDev <Suslova-valerija@rambler.ru>
2022-09-27 00:15:28 +0300

    add more examples

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import Modal from "./UI/Modal/Modal";
 import { useState } from "react";
 import ClickToXHide from "./examples/ClickToXHide";
 import ClickToItself from "./examples/ClickToItself";
+import ClickToButtonHide from "./examples/ClickToButtonHide";
 
 function App() {
   const [isOpenOne, setIsOpenOne] = useState(false);
   const [isOpenTwo, setIsOpenTwo] = useState(false);
+  const [isOpenThree, setIsOpenThree] = useState(false);
 
   const handleCloseOne = () => {
     setIsOpenOne(false);
   };
   const handleCloseTwo = () => {
     setIsOpenTwo(false);
+  };
+  const handleCloseThree = () => {
+    setIsOpenThree(false);
   };
 
   const handleOpenModalOne = () => {
@@ -20,19 +25,30 @@ function App() {
   const handleOpenModalTwo = () => {
     setIsOpenTwo(true);
   };
+  const handleOpenModalThree = () => {
+    setIsOpenThree(true);
+  };
 
   return (
     <div>
       <h1>Few examples of using my modal)</h1>
+      <h3>Try from left to right and see the code</h3>
       <button onClick={handleOpenModalOne}>
         Open a Modal that closes when 'X' is clicked
       </button>
       <button onClick={handleOpenModalTwo}>
-        Open a Modal that closes when itself is cliced
+        Open a Modal that closes when itself is clicked
       </button>
+      <button onClick={handleOpenModalThree}>
+        Open a Modal that closes when button is clicked
+      </button>
+      <button onClick={handleCloseThree}>Button</button>
       {isOpenOne && <Modal onClose={handleCloseOne} Component={ClickToXHide} />}
       {isOpenTwo && (
         <Modal onClose={handleCloseTwo} Component={ClickToItself} />
+      )}
+      {isOpenThree && (
+        <Modal onClose={handleCloseThree} Component={ClickToButtonHide} />
       )}
     </div>
   );
---
+++ src/examples/ClickToButtonHide.js
@@ -0,0 +1,7 @@
+import "./ClickToButton.module.css";
+
+const ClickToButtonHide = ({ onClose }) => {
+  return <h2>Example_3. Click on button 'Button' to close the Modal</h2>;
+};
+
+export default ClickToButtonHide;
