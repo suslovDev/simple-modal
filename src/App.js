4c11d63dbead16bf208614cc822cd774df8eb40e
SuslovDev <Suslova-valerija@rambler.ru>
2023-05-03 06:28:55 +0300

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import Modal from "./Modal";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+let start = 0;
+let end = 0;
+let step = 10;
 
-const Com = () => {
-  return <h1>djfhdjhfjdhf</h1>;
-};
 const App = () => {
-  const [modalActive, setModalActive] = useState(true);
+  const arr = [];
+  for (let i = 0; i < 1000; i++) {
+    arr.push(i);
+  }
+
+  const [currentSlice, setCurrentSlice] = useState(arr);
+  const [step, setStep] = useState(10);
+  const [start, setStart] = useState(0);
+  const [end, setEnd] = useState(step);
+
+  useEffect(() => {
+    setCurrentSlice(arr.slice(start, end));
+  }, [start, end, step]);
+
+  const handleNext = () => {
+    setStart((prev) => prev + step);
+
+    setEnd((prev) => prev + step);
+  };
+
+  const handleSetStep = () => {
+    setStep(step+1);
+  };
+
   return (
-    <Modal active={modalActive} setActive={setModalActive} Component={Com} />
+    <>
+      <button onClick={handleNext}>Next Slice</button>
+      <button onClick={handleSetStep}>Add shown elements</button>
+      {currentSlice.map((item) => (
+        <h2>{item}</h2>
+      ))}
+    </>
   );
 };
 
